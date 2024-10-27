5077e9f733b0f7aea97414414df07d4162cacadb
Sagar Raval <62638370+SRaval20@users.noreply.github.com>
2024-10-27 10:41:35 -0700

    Update FlattenBinaryTreeToLinkedList.java

--- Premium/FlattenBinaryTreeToLinkedList.java
+++ Premium/FlattenBinaryTreeToLinkedList.java
@@ -1,3 +1,66 @@
+/*
+453 · Flatten Binary Tree to Linked List
+Algorithms
+Easy
+Accepted Rate
+46%
+
+
+Description
+Solution69
+Notes
+Discuss99+
+Leaderboard
+Record
+
+Description
+Flatten a binary tree to a fake "linked list" in pre-order traversal.
+
+Here we use the right pointer in TreeNode as the next pointer in ListNode.
+
+Only $39.9 for the "Twitter Comment System Project Practice" within a limited time of 7 days!
+
+WeChat Notes Twitter for more information（WeChat ID jiuzhangfeifei）
+
+
+
+Don't forget to mark the left child of each node to null. Or you will get Time Limit Exceeded or Memory Limit Exceeded.
+
+Example
+Example 1:
+
+Input:{1,2,5,3,4,#,6}
+Output：{1,#,2,#,3,#,4,#,5,#,6}
+Explanation：
+     1
+    / \
+   2   5
+  / \   \
+ 3   4   6
+
+1
+\
+ 2
+  \
+   3
+    \
+     4
+      \
+       5
+        \
+         6
+Example 2:
+
+Input:{1}
+Output:{1}
+Explanation：
+         1
+         1
+Challenge
+Do it in-place without any extra memory.
+*/
+
+
 // Time Complexity:  O(n)
 // Space Complexity: O(h)
 
